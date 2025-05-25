7aeb480e68424df31332511ebde6efccf076ae24
ismaildx <ismaildxx@gmail.com>
2025-05-25 21:08:56 +0400

    added apis

--- src/components/AskAIButton.tsx
+++ src/components/AskAIButton.tsx
@@ -1,14 +1,81 @@
 "use client";
 
 import { User } from "@supabase/supabase-js";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 type Props = {
   user: User | null;
 };
 
 const AskAIButton = ({ user }: Props) => {
-  console.log("user email", user?.email);
-  return <div>AskAIButton</div>;
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [questionText, setQuestionText] = useState("");
+  const [questions, setQuestions] = useState<string[]>([]);
+  const [responses, setResponses] = useState<string[]>([]);
+
+  const handleOnOpenChange = (isOpen: boolean) => {
+    if (!user) {
+      router.push("/login");
+    } else {
+      if (isOpen) {
+      }
+      setOpen(isOpen);
+    }
+  };
+  return (
+    <Dialog open={open} onOpenChange={handleOnOpenChange}>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input
+              id="username"
+              defaultValue="@peduarte"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default AskAIButton;
